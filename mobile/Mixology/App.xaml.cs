f0adb2baf2e7e97f190bdc74da8fb843f0d9042c
Ania <coryakina1anna@gmail.com>
2025-12-25 14:01:59 +0500

    Проверка токена пользователя и редирект

--- mobile/Mixology/App.xaml.cs
+++ mobile/Mixology/App.xaml.cs
@@ -1,14 +1,70 @@
-﻿namespace Mixology;
+﻿using Mixology.Services;
+
+namespace Mixology;
 
 public partial class App : Application
 {
-    public App()
+    private readonly UserService _userService;
+    public App(UserService userService)
     {
         InitializeComponent();
+        _userService = userService;
     }
 
+    private async void CheckUserAsync(AppShell shell)
+    {
+        string? token = null;
+        try
+        {
+            token = await SecureStorage.Default.GetAsync("token");
+        }
+        catch {}
+
+        if (string.IsNullOrWhiteSpace(token))
+        {
+            MainPage = new AppShell();
+            await Shell.Current.GoToAsync("//login");
+            return;
+        }
+        try
+        {
+            var user = await _userService.GetCurrentUserInfo(token);
+            if (user != null)
+            {
+                MainPage = new AppShell();
+                await Shell.Current.GoToAsync("//main");
+            }
+            else
+            {
+                await RedirectToLoginAsync();
+            }
+        }
+        catch (HttpRequestException ex) when (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        {
+            await RedirectToLoginAsync();
+        }
+        catch
+        {
+            await RedirectToLoginAsync();
+        }
+    }
+    
+    private async Task RedirectToLoginAsync()
+    {
+        try
+        {
+            await SecureStorage.Default.SetAsync("token", string.Empty);
+        }
+        catch { }
+
+        MainPage = new AppShell();
+        await Shell.Current.GoToAsync("//login");
+    }
+    
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AppShell());
+        var shell = new AppShell();
+        CheckUserAsync(shell);     
+        return new Window(shell);
     }
 }
